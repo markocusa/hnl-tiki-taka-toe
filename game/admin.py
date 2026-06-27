44909422c5ad1ddb0fc5f531a246feebe4a58682
VanjaV10 <vv55136@fer.hr>
2026-06-27 16:24:56 +0200

    Fix baze

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Confederation, Country, Club, Player
 
 # Register your models here.
+admin.site.register(Confederation)
+admin.site.register(Country)
+admin.site.register(Club)
+admin.site.register(Player)
--- game/models.py
+++ game/models.py
@@ -1,24 +1,36 @@
 from django.db import models
 
 # Create your models here.
-class Drzava(models.Model):
+class Confederation(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 
-class Klub(models.Model):
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    continent = models.ForeignKey(
+        Confederation,
+        on_delete=models.CASCADE,
+        related_name="countries"
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Club(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 
-class Igrac(models.Model):
+class Player(models.Model):
     name = models.CharField(max_length=100)
-    clubs = models.ManyToManyField(Klub)
-    country = models.ForeignKey(Drzava,on_delete=models.CASCADE)
+    clubs = models.ManyToManyField(Club)
+    country = models.ForeignKey(Country,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
